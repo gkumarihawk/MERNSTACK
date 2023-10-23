c5b295ad7451e17ebd795ddb5ef2e3bb535bc9ef
gkumarihawk <74431929+gkumarihawk@users.noreply.github.com>
2023-10-23 11:14:46 -0500

    new file

---
+++ dataTypes.js
@@ -0,0 +1,55 @@
+//Number
+
+var age = 18;
+var price = 328.90;
+
+console.log(typeof age);
+console.log(typeof price);
+
+// String
+
+var name = "Gauri";
+var message = 'Good morning';
+
+console.log(typeof name);
+console.log(typeof message);
+
+//Boolean
+
+var istrue = true;
+var isfalse = false;
+
+console.log(typeof istrue);
+console.log(typeof isfalse);
+
+//object
+
+var me = {
+    firstName: 'Gauri',
+    lastName: 'Kumari',
+    age: 23,
+  };
+
+console.log(typeof me);
+
+//Undefined
+
+var x;
+console.log(typeof x);
+
+//Null
+
+var y = null;
+console.log(typeof y);
+
+//Dynamic typing:
+
+let myVariable = 42;  // myVariable is a number
+console.log(typeof myVariable);
+
+myVariable = 'Hello'; // myVariable is now a string
+console.log(typeof myVariable);
+
+myVariable = true;    // myVariable is now a boolean
+console.log(typeof myVariable);
+
